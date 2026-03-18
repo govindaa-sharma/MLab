31b11dec10dd83a4c431682328da78a335c735d4
govindaa-sharma <gs2083409@gmail.com>
2026-03-18 09:14:32 +0530

    fixing database bug

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:siuuU123@localhost:5432/experiment_db"
-
+DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
